d774c803e8a518e80dbc34c4b12028c055469087
laalm99 <95426832+laalm99@users.noreply.github.com>
2023-02-09 10:29:49 +0300

    Canvas UI Edits

---
+++ pong-project/Assets/Scripts/FloatingLean.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingLean : MonoBehaviour
+{
+    [SerializeField] float time;
+    [SerializeField] float to;
+
+    void Start()
+    {
+        LeanTween.moveY(this.gameObject, to, time).setLoopPingPong();
+
+       
+    }
+}
